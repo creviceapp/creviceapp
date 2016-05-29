8b6608421dd438f0e652c8f9fd3427949c1a6ea7
rubyu <ruby.u.g@gmail.com>
2016-05-29 14:49:15 +0900

    Add tests for WindowsApplication

---
+++ CreviceAppTests/WindowsApplicationTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CreviceApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreviceApp.Tests
+{
+    [TestClass()]
+    public class WindowsApplicationTests
+    {
+        [TestMethod()]
+        public void GetForegroundTest()
+        {
+            var app = new WindowsApplication();
+            var result = app.GetForeground();
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void GetOnCursorTest()
+        {
+            var app = new WindowsApplication();
+            var result = app.GetOnCursor(0, 0);
+            Assert.IsNotNull(result);
+        }
+    }
+}
