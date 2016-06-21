b504b0c9fefb3398834a36d0e524e40329d91078
rubyu <ruby.u.g@gmail.com>
2016-06-21 19:12:04 +0900

    Add a missing file

---
+++ CreviceApp/Core.AppGlobal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CreviceApp
+{
+    public class AppGlobal
+    {
+        public readonly Core.Config.UserConfig UserConfig;
+        public readonly MainForm MainForm;
+
+        public AppGlobal()
+        {
+            this.UserConfig = new Core.Config.UserConfig();
+            this.MainForm = new MainForm(this);
+        }
+    }
+}
