c1ebabe3e9dd0f64b0f0a961f944baddf975a096
rubyu <ruby.u.g@gmail.com>
2016-06-22 12:56:02 +0900

    Refine tests for Core.State0

--- CreviceAppTests/Core.FSM.State0Tests.cs
+++ CreviceAppTests/Core.FSM.State0Tests.cs
@@ -73,9 +73,18 @@ namespace CreviceApp.Core.FSM.Tests
         }
 
         [TestMethod()]
-        public void InputMustExecuteTransition00Test()
+        public void InputMustExecuteTransition00WhenHavingIfButtonGestureDefinitionAndISingleActionGivenTest()
         {
-            foreach (var a in TestDef.Constant.AcceptablesInIfButtonClause)
+            var list = new List<DSL.Def.AcceptableInIfButtonClause>()
+            {
+                DSL.Def.Constant.WheelUp,
+                DSL.Def.Constant.WheelDown,
+                DSL.Def.Constant.WheelLeft,
+                DSL.Def.Constant.WheelRight
+            };
+            Assert.IsTrue(list.TrueForAll(x => TestDef.Constant.AcceptablesInIfButtonClause.Contains(x)));
+
+            foreach (var a in  list)
             {
                 var countDown = new CountdownEvent(1);
                 var gestureDef = new List<GestureDefinition>() {
@@ -89,26 +98,9 @@ namespace CreviceApp.Core.FSM.Tests
                 {
                     countDown.Reset();
                     var ev = Helper.Convert(b);
-                    if (ev is Def.Event.ISingleAction)
-                    {
-                        var res = S0.Input(ev, new Point());
-                        Assert.IsTrue(res.NextState is State0);
-                    }
-                    else if (ev is Def.Event.IDoubleActionSet)
-                    {
-                        var ev_pair = (Def.Event.IEvent)(ev as Def.Event.IDoubleActionSet).GetPair();
-                        var res = S0.Input(ev, new Point());
-                        if (a == b)
-                        {
-                            Assert.IsTrue(res.NextState is State1);
-                        }
-                        else
-                        {
-                            Assert.IsTrue(res.NextState is State0);
-                        }
-                        res = res.NextState.Input(ev_pair, new Point());
-                        Assert.IsTrue(res.NextState is State0);
-                    }
+                    var res = S0.Input(ev, new Point());
+                    Assert.IsTrue(res.NextState is State0);
+                    Assert.IsFalse(res.StrokeWatcher.IsResetRequested);
                     if (a == b)
                     {
                         Assert.IsTrue(countDown.Wait(50));
@@ -122,7 +114,47 @@ namespace CreviceApp.Core.FSM.Tests
         }
 
         [TestMethod()]
-        public void InputMustExecuteTransition01Test()
+        public void InputMustExecuteTransition01WhenHavingIfButtonGestureDefinitionAndIDoubleActionGivenTest()
+        {
+            var list = new List<DSL.Def.AcceptableInIfButtonClause>()
+            {
+                DSL.Def.Constant.LeftButton,
+                DSL.Def.Constant.MiddleButton,
+                DSL.Def.Constant.RightButton,
+                DSL.Def.Constant.X1Button,
+                DSL.Def.Constant.X2Button
+            };
+            Assert.IsTrue(list.TrueForAll(x => TestDef.Constant.AcceptablesInIfButtonClause.Contains(x)));
+
+            foreach (var a in list)
+            {
+                var gestureDef = new List<GestureDefinition>() {
+                new IfButtonGestureDefinition(
+                    (ctx) => { return true; },
+                    a,
+                    (ctx) => { })
+                };
+                var S0 = new State0(new StateGlobal(), gestureDef);
+                foreach (var b in TestDef.Constant.AcceptablesInIfButtonClause)
+                {
+                    var res = S0.Input(Helper.Convert(b), new Point());
+                    if (a == b)
+                    {
+                        Assert.IsTrue(res.NextState is State1);
+                        Assert.IsTrue(res.StrokeWatcher.IsResetRequested);
+                        Assert.AreEqual(gestureDef[0], ((State1)res.NextState).T5.ToList()[0]);
+                    }
+                    else
+                    {
+                        Assert.IsTrue(res.NextState is State0);
+                        Assert.IsFalse(res.StrokeWatcher.IsResetRequested);
+                    }
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void InputMustExecuteTransition01WhenHavingOnButtonIfButtonGestureDefinitionAndIDoubleActionGivenTest()
         {
             foreach (var a in TestDef.Constant.AcceptablesInOnClause)
             {
@@ -140,10 +172,13 @@ namespace CreviceApp.Core.FSM.Tests
                     if (a == b)
                     {
                         Assert.IsTrue(res.NextState is State1);
+                        Assert.IsTrue(res.StrokeWatcher.IsResetRequested);
+                        Assert.IsTrue(gestureDef.SequenceEqual(((State1)res.NextState).T2[Def.Constant.WheelUp]));
                     }
                     else
                     {
                         Assert.IsTrue(res.NextState is State0);
+                        Assert.IsFalse(res.StrokeWatcher.IsResetRequested);
                     }
                 }
             }
