8d822cf4d820127a6a7a84b20f58320de20287eb
Unknown <tukigase@gmail.com>
2018-02-11 13:22:23 +0900

    Fix an awful bug ContextManager.Evalute always returned true.

--- CreviceApp/GM.ContextManager.cs
+++ CreviceApp/GM.ContextManager.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace Crevice.GestureMachine
 {
     using System.Drawing;
+    using Crevice.Logging;
     using Crevice.Core.Context;
     using Crevice.Core.DSL;
 
@@ -28,7 +29,16 @@ namespace Crevice.GestureMachine
             {
                 return whenElement.WhenEvaluator(evalContext);
             });
-            return task.Wait(100) && task.Result;
+
+            try
+            {
+                return task.Wait(100) && task.Result;
+            }
+            catch (Exception ex)
+            {
+                Verbose.Error("An exception was thrown while evaluating an evaluator: {0}", ex.ToString());
+                return false;
+            }
         }
 
         public override void Execute(
