af831d9f8b7c3fa083e999c626ae293ffbbfced4
Unknown <tukigase@gmail.com>
2018-08-06 17:16:53 +0900

    Fix an issue that Tooltip's duration was not assured.

--- CreviceApp/UI.TooltipNotifier.cs
+++ CreviceApp/UI.TooltipNotifier.cs
@@ -13,12 +13,13 @@ namespace Crevice.UI
     {
         private const int Absolute = 0x0002;
 
-        private IWin32Window win;
-        private MethodInfo SetTool;
-        private MethodInfo SetTrackPosition;
-        private MethodInfo StartTimer;
+        private readonly IWin32Window win;
+        private readonly MethodInfo SetTool;
+        private readonly MethodInfo SetTrackPosition;
+        private readonly MethodInfo StartTimer;
+        private readonly MethodInfo StopTimer;
 
-        private ToolTip tooltip = new ToolTip();
+        private readonly ToolTip tooltip = new ToolTip();
 
         public TooltipNotifier(IWin32Window win)
         {
@@ -26,10 +27,12 @@ namespace Crevice.UI
             this.SetTool = tooltip.GetType().GetMethod("SetTool", BindingFlags.Instance | BindingFlags.NonPublic);
             this.SetTrackPosition = tooltip.GetType().GetMethod("SetTrackPosition", BindingFlags.Instance | BindingFlags.NonPublic);
             this.StartTimer = tooltip.GetType().GetMethod("StartTimer", BindingFlags.Instance | BindingFlags.NonPublic);
+            this.StopTimer = tooltip.GetType().GetMethod("StopTimer", BindingFlags.Instance | BindingFlags.NonPublic);
         }
 
         public void Show(string text, Point point, int duration)
         {
+            StopTimer.Invoke(tooltip, new object[] { win });
             SetTrackPosition.Invoke(tooltip, new object[] { point.X, point.Y });
             SetTool.Invoke(tooltip, new object[] { win, text, Absolute, point });
             StartTimer.Invoke(tooltip, new object[] { win, duration });
